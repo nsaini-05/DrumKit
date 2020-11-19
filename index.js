241bb684e1f86ee32a690f7bddec6f420d819280
neeraj78 <saini.neeraj7788@gmail.com>
2020-11-19 08:15:09 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,86 @@
+
+var buttons = document.querySelectorAll(".drum");
+
+
+
+for (i = 0 ; i < buttons.length ; i++)
+{
+  buttons[i].addEventListener("click", function ()
+  {
+    play(this.innerHTML);
+    buttonAnimation(this.innerHTML);
+});
+}
+
+
+
+
+
+
+
+document.addEventListener("keypress", function(event){
+
+play(event.key);
+buttonAnimation(event.key);
+
+
+});
+
+
+function play(key)
+{
+
+  var buttonInnerHTML = key;
+  console.log(buttonInnerHTML);
+  switch (buttonInnerHTML) {
+    case "w":
+    var audio = new Audio("sounds/tom-4.mp3");
+    audio.play();
+    break;
+
+    case "a":
+    var audio = new Audio("sounds/tom-3.mp3");
+    audio.play();
+    break;
+
+    case "s":
+    var audio = new Audio("sounds/tom-2.mp3");
+    audio.play();
+    break;
+
+    case "d":
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+    break;
+
+    case "j":
+    var audio = new Audio("sounds/snare.mp3");
+    audio.play();
+    break;
+
+    case "k":
+    var audio = new Audio("sounds/kick-bass.mp3");
+    audio.play();
+    break;
+
+    case "l":
+    var audio = new Audio("sounds/crash.mp3");
+    audio.play();
+    break;
+    default:
+}};
+
+
+function buttonAnimation(key)
+{
+  var activeButton = document.querySelector("."+key);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){activeButton.classList.remove("pressed");} , 50);
+
+
+
+
+
+
+}
